d7217c9ae744f7048550d310017c35c3cd0f36f1
ottomicheletti <michelettiotto@gmail.com>
2022-06-20 18:20:53 -0300

    feat(index.js): requisito 01

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 app.use(bodyParser.json());
 
+const { readFile, writeFile } = require('./helpers/readWriteFile');
+
 const HTTP_OK_STATUS = 200;
-const PORT = '3001';
+const PORT = '3000';
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -14,4 +15,13 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = await readFile();
+  if (data.lenght === 0) {
+    res.status(200).json([]);
+  } else {
+    res.status(200).json(data);
+  }
 });
