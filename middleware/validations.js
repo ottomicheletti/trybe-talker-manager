45e3a71fa12da1d625c6d9794bd80f9800ba81cf
ottomicheletti <michelettiotto@gmail.com>
2022-06-20 19:34:27 -0300

    fix(validations.js): arruma a linha 14

--- middleware/validations.js
+++ middleware/validations.js
@@ -11,7 +11,7 @@ const validateLogin = (req, res, next) => {
   if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
-  if (password < 6) {
+  if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
